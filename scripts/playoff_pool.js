8cb834c04a7f5f678321a71a7c42c0cfbc874058
corvettes13 <86648326+corvettes13@users.noreply.github.com>
2026-05-19 17:43:12 -0400

    Update playoff_pool.js

--- scripts/playoff_pool.js
+++ scripts/playoff_pool.js
@@ -1,5 +1,5 @@
 // 1. CONFIGURATION & STATE
-const PLAYOFF_TEAMS = ['BUF', 'MTL', 'CAR', 'PHI', 'COL', 'MIN', 'VGK', 'ANA'];
+const PLAYOFF_TEAMS = ['MTL', 'CAR', 'COL', 'VGK'];
 
 const skaterStatIdMap = {
     0: 'GP', 1: 'G', 2: 'A', 3: 'PTS', 4: 'PlusMinus', 5: 'PIM',
