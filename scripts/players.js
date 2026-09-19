6ed0bc4b9dccd0879a9284772ba0fb942222ac83
corvettes13 <86648326+corvettes13@users.noreply.github.com>
2026-09-19 12:58:11 -0400

    2026 Player Projection Update

--- scripts/players.js
+++ scripts/players.js
@@ -373,9 +373,9 @@ function loadSeasonStats(seasonKey) {
       skater: "/fantasy-hockey/data/2026_skater_stats.json",
       goalie: "/fantasy-hockey/data/2026_goalie_stats.json"
     },
-    "2025_projections": {
-      skater: "/fantasy-hockey/data/2025_skater_proj.json",
-      goalie: "/fantasy-hockey/data/2025_goalie_proj.json"
+    "2026_projections": {
+      skater: "/fantasy-hockey/data/2026_skater_proj.json",
+      goalie: "/fantasy-hockey/data/2026_goalie_proj.json"
     }
   };
 
